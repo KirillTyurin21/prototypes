38b1c8f1342238814b9c41dca3e55e69892e5615
KirillTyurin21 <KirillTyurin21@users.noreply.github.com>
2026-03-11 12:11:25 +0400

    chore(web-screens): release v1.2

--- src/app/prototypes/web-screens/changelog.data.ts
+++ src/app/prototypes/web-screens/changelog.data.ts
@@ -4,7 +4,7 @@ export const CHANGELOG: ChangelogRelease[] = [
   {
     version: '1.2',
     date: '2026-03-11',
-    status: 'unreleased',
+    status: 'released',
     changes: [
       {
         page: 'Настройки дисплея',
