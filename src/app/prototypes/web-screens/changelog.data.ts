0712449ad8d0cc844baac93784789b0b997b53b6
KirillTyurin21 <KirillTyurin21@users.noreply.github.com>
2026-05-07 03:25:00 +0400

    chore(web-screens): mark v1.16 as released

--- src/app/prototypes/web-screens/changelog.data.ts
+++ src/app/prototypes/web-screens/changelog.data.ts
@@ -3,8 +3,8 @@ import { ChangelogRelease } from '@/shared/changelog.types';
 export const CHANGELOG: ChangelogRelease[] = [
   {
     version: '1.16',
-    date: '2026-05-06',
-    status: 'unreleased',
+    date: '2026-05-07',
+    status: 'released',
     changes: [
       {
         page: 'Обработчики событий (Звуки)',
