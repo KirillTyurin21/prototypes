183f3918c07de20e644acd5fd58f8900faf186e9
KirillTyurin21 <KirillTyurin21@users.noreply.github.com>
2026-05-06 13:35:14 +0400

    chore: changelog 1.15 released

--- src/app/prototypes/web-screens/changelog.data.ts
+++ src/app/prototypes/web-screens/changelog.data.ts
@@ -4,7 +4,7 @@ export const CHANGELOG: ChangelogRelease[] = [
   {
     version: '1.15',
     date: '2026-05-06',
-    status: 'unreleased',
+    status: 'released',
     changes: [
       {
         page: 'Обработчики событий (Звуки)',
