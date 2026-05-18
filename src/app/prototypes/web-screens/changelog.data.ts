fed31f43c826d613e3b7134eae376f2efbc41429
KirillTyurin21 <KirillTyurin21@users.noreply.github.com>
2026-05-18 15:51:08 +0400

    release(web-screens): 1.20 released

--- src/app/prototypes/web-screens/changelog.data.ts
+++ src/app/prototypes/web-screens/changelog.data.ts
@@ -4,7 +4,7 @@ export const CHANGELOG: ChangelogRelease[] = [
   {
     version: '1.20',
     date: '2026-05-18',
-    status: 'unreleased',
+    status: 'released',
     changes: [
       {
         page: 'Конструктор тем Arrivals',
