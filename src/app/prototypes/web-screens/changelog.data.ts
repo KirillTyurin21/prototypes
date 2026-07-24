4e4058f4543617e20bb3e223d327e434008581fe
KirillTyurin21 <KirillTyurin21@users.noreply.github.com>
2026-07-24 12:26:31 +0400

    chore(web-screens): mark v1.38 as released

--- src/app/prototypes/web-screens/changelog.data.ts
+++ src/app/prototypes/web-screens/changelog.data.ts
@@ -4,7 +4,7 @@ export const CHANGELOG: ChangelogRelease[] = [
   {
     version: '1.38',
     date: '2026-07-24',
-    status: 'unreleased',
+    status: 'released',
     changes: [
       {
         page: 'Темы киоска',
